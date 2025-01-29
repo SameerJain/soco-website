8e2b0010d7fcaeaf6b3357ab0aa3ccc7be195e52
SameerJain <114765510+SameerJain@users.noreply.github.com>
2025-01-29 15:51:18 -0500

    added formatting and some docs

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
-// Add mouse movement effect to cards
+// Add mouse lightup effect to cards
 document.querySelectorAll('.card').forEach(card => {
     card.addEventListener('mousemove', e => {
+    // get box of card
         const rect = card.getBoundingClientRect();
+    
+    // get coordinates relative to card
         const x = ((e.clientX - rect.left) / rect.width) * 100;
         const y = ((e.clientY - rect.top) / rect.height) * 100;
-
+// update
         card.style.setProperty('--mouse-x', `${x}%`);
         card.style.setProperty('--mouse-y', `${y}%`);
     });
